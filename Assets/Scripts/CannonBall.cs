6b135cc53bae98661211cb065a82552d8bf69c63
Steve Rector <steverector27@gmail.com>
2022-11-29 17:51:39 -0800

    finishes class 2

--- Assets/Scripts/CannonBall.cs
+++ Assets/Scripts/CannonBall.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CannonBall : MonoBehaviour
 {
+    private static readonly int Exploded = Animator.StringToHash("Exploded");
     private Rigidbody _ballRigidBody;
+
+    [SerializeField] private Animator _animator;
 
     void Awake()
     {
@@ -18,5 +21,26 @@ public class CannonBall : MonoBehaviour
             Random.Range(-10f, 10f),
             Random.Range(-10f, 10f), 
             Random.Range(-10f, 10f));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        transform.rotation = Quaternion.FromToRotation(transform.up, collision.GetContact(0).normal);
+
+        _ballRigidBody.velocity = Vector3.zero;
+        _ballRigidBody.angularVelocity = Vector3.zero;
+        _ballRigidBody.isKinematic = true;
+
+        _animator.SetTrigger(Exploded);
+    }
+
+    public void OnFinishedExplosionAnimation()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] private LayerMask _waterTriggerLayer;
+    private void OnTriggerEnter(Collider other)
+    {
+        // uses bitwise operation to check if the other colliders layer is a layer that can be collided with 
+        if ((_waterTriggerLayer.value & (1 << other.gameObject.layer)) > 0)
+        {
+            Debug.Log("suh");
+            Destroy(gameObject);
+        }
+    }
+}
