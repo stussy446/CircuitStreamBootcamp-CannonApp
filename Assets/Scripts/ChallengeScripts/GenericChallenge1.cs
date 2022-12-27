4685ae54f0ce3bb5250750120e8f72c8e94ab8a7
Steve Rector <steverector27@gmail.com>
2022-12-26 16:03:33 -0800

    Finished base of assignment, objects being created with type of component attached

--- Assets/Scripts/ChallengeScripts/GenericChallenge1.cs
+++ Assets/Scripts/ChallengeScripts/GenericChallenge1.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GenericChallenge1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _genericprefab;
+
+    private void Start()
     {
-        
+        InstantiateWithComponent<Animator>();
+        InstantiateWithComponent<Target>();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Instantiates a genericprefab game object and adds whatever component type is passed into the method. After it is added, sets the component to false and 
+    /// logs the components addition to the gameobject to the console.
+    /// </summary>
+    /// <typeparam name="T">Type of component to be added to the game object</typeparam>
+    private void InstantiateWithComponent<T>() where T : Component
     {
-        
+        var newObject = Instantiate(_genericprefab, transform);
+        var newComponent = newObject.AddComponent<T>();
+
+        newComponent.gameObject.SetActive(false);
+        Debug.Log($"Object initialized, component {typeof(T)} has been added to the object");
     }
 }
